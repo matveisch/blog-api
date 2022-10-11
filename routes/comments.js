7acf5b286331f5e785c5b55b3f25215ecfb377ec
Matvei <matvesch@protonmail.com>
2022-10-11 18:49:16 +0300

    fixes

--- routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const comment_controller = require('../controllers/commentController');
-
-const router = express.Router();
-
-router.post('/', comment_controller.create_comment);
-
-module.exports = router;
