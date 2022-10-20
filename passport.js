df970c94ce52be2b864841679aeebd779c2d883c
Matvei <matvesch@protonmail.com>
2022-10-20 10:18:43 +0300

    fixes

--- passport.js
+++ passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
 const bcrypt = require('bcrypt');
 const passportJWT = require("passport-jwt");
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
+
+const User = require('./models/user');
 
 passport.use(new LocalStrategy({
         usernameField: 'username',
